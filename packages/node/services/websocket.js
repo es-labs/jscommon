03bdb786a327c722bc9b3aff6313ea088a9a35b6
Aaron_Gong <Aaron_Gong@dell.com>
2023-08-30 12:09:39 +0800

    fix websocket

--- packages/node/services/websocket.js
+++ packages/node/services/websocket.js
@@ -5,114 +5,6 @@
 const WebSocket = require('ws')
 const https = require('https')
 
-/*
-let wss
-let onClientConnect = (ws) => { 
-  // console.log('client connected')
-}
-let onClientClose = (ws) => { 
-  // console.log('client disconnected')
-}
-let onClientMessage = async (data, isBinary, ws, _wss) => { // client incoming message
-  const message = isBinary ? data : data.toString()
-  // console.log('message', message)
-  try { // try-catch only detect immediate error, cannot detect if write failure    
-    if (_wss) { // send to other clients except self
-      _wss.clients.forEach((client) => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(message) // send message to others 
-        }
-      })
-      // ws.send('something', function ack(error) { console.log }) // If error !defined, send has been completed, otherwise error object will indicate what failed.
-      ws.send(message) // echo back message...
-    }
-  } catch (e) {
-    console.log(e.toString())
-  }
-}
-
-exports.get = () => wss // get wss so that you can send messages to other clients...
-
-exports.send = (data) => {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data)
-    }
-  })
-}
-
-exports.open = (server=null, app=null) => {
-  const { WS_KEEEPALIVE_MS, WS_PORT } = process.env
-  const { HTTPS_PRIVATE_KEY, HTTPS_CERTIFICATE } = process.env
-  let err
-  try {
-    if (!wss && WS_PORT) {
-      if (HTTPS_CERTIFICATE) {
-        if (!server) server = https.createServer({
-          key: HTTPS_PRIVATE_KEY, cert: HTTPS_CERTIFICATE
-        }).listen(WS_PORT) // use same port, create server because of graphql subscriptions
-        wss = new WebSocket.Server({ server })
-      } else {
-        if (!server) wss = new WebSocket.Server({ port: WS_PORT }) // use seperate port
-        else wss = new WebSocket.Server({ server })
-      }
-      if (app) server.on('request', app)
-
-      console.log('WS API listening on port ' + WS_PORT)
-      if (wss) {
-        wss.on('connection', (ws) => {
-          // console.log('ws client connected')
-          onClientConnect(ws) // what else to do when client connects
-          ws.isAlive = true
-          ws.on('pong', () => { ws.isAlive = true })
-          ws.on('close', () => onClientClose(ws))
-          ws.on('message', (data, isBinary) => onClientMessage(data, isBinary, ws, wss))
-        })
-        setInterval(() => { // set keep-alive
-          // console.log('WS Clients: ', wss.clients.size)
-          wss.clients.forEach((ws) => {
-            if (!ws.isAlive) return ws.terminate() // force close
-            ws.isAlive = false
-            return ws.ping(() => {}) // NOSONAR
-          })
-        }, WS_KEEEPALIVE_MS)
-      }
-    } else {
-      console.log('NO WS Service To Open')
-    }
-  } catch (e) {
-    err = e.toString()
-  }
-  console.log('WS Open ' + (err ? err : 'Done'))
-  return this
-}
-
-exports.close = function () {
-  try { // close all connections
-    if (wss) {
-      wss.close()
-      // wss.clients.forEach(client => client.close(0, 'wss close() called')) // close gracefully
-      for (const client of wss.clients) client.terminate() // https://github.com/websockets/ws/releases/tag/8.0.0
-      wss = null //delete wss
-    }
-  } catch (e) {
-    console.error(e.toString())
-  }
-  console.log('WS API CLOSE OK')
-}
-
-exports.setOnClientMessage = function (onClientMessageFn) {
-  onClientMessage = onClientMessageFn
-}
-
-exports.setOnClientConnect = function (onClientConnectFn) { //  what to do when client connects
-  onClientConnect = onClientConnectFn
-}
-
-exports.setOnClientCLose = function (onClientCloseFn) { //  what to do when client closes
-  onClientClose = onClientCloseFn
-}
-
 // NOSONAR
 // function heartbeat() {
 //   clearTimeout(this.pingTimeout)
@@ -132,7 +24,6 @@ exports.setOnClientCLose = function (onClientCloseFn) { //  what to do when clie
 // // Create web socket server on top of a regular http server
 // let wss = new WSServer({ server })
 // server.on('request', app)
-*/
 
 module.exports = class Wss {
   constructor(options = JSON.parse(process.env.WS_OPTIONS || null) || {}) {
@@ -208,11 +99,11 @@ module.exports = class Wss {
         if (this._wss) {
           this._wss.on('connection', (ws) => {
             // console.log('ws client connected')
-            onClientConnect(ws) // what else to do when client connects
+            this._onClientConnect(ws) // what else to do when client connects
             ws.isAlive = true
             ws.on('pong', () => { ws.isAlive = true })
-            ws.on('close', () => onClientClose(ws))
-            ws.on('message', (data, isBinary) => onClientMessage(data, isBinary, ws, this._wss))
+            ws.on('close', () => this._onClientClose(ws))
+            ws.on('message', (data, isBinary) => this._onClientMessage(data, isBinary, ws, this._wss))
           })
           setInterval(() => { // set keep-alive
             // console.log('WS Clients: ', this._wss.clients.size)
