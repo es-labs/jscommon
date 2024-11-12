b98e7042556cd0d54724f5b425ac7851532b79d8
ais-one <aaronjxz@gmail.com>
2024-11-12 11:24:00 +0800

    wip

--- tools/dbdeploy/dbs/express-template/migrations/20230827094947_initial.js
+++ tools/dbdeploy/dbs/express-template/migrations/20230827094947_initial.js
@@ -120,19 +120,20 @@ exports.up = async function(knex) {
     //   { column: "time", order: "DESC" },
     // ]); // TBD indexing
   })
+
   await knex.schema.createTable('audit_logs', (table) => {
-	table.increments('id').primary()
-  table.string('user')
-	table.datetime('timestamp')
-	table.string('db_name')
-	table.string('table_name')
-	table.string('op').comment('READ, UPDATE, DELETE, INSERT')
-	table.string('where_cols')
-	table.string('where_vals')
-	table.string('cols_changed')
-	table.text('prev_values')
-	table.text('new_values')
-	table.index(['timestamp', 'db_name', 'op'])
+    table.increments('id').primary()
+    table.string('user')
+    table.datetime('timestamp')
+    table.string('db_name')
+    table.string('table_name')
+    table.string('op').comment('READ, UPDATE, DELETE, INSERT')
+    table.string('where_cols')
+    table.string('where_vals')
+    table.string('cols_changed')
+    table.text('prev_values')
+    table.text('new_values')
+    table.index(['timestamp', 'db_name', 'op'])
   })
 }
 
@@ -150,6 +151,6 @@ exports.down = async function(knex) {
   await knex.schema.dropTableIfExists('state')
   await knex.schema.dropTableIfExists('student')
   await knex.schema.dropTableIfExists('student_subject')
-  await knex.schema.dropTableIfExists('users')
   await knex.schema.dropTableIfExists('audit_logs')
+  await knex.schema.dropTableIfExists('users')
 }
