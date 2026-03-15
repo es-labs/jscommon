a0addec2ffc3f1f22e854625c350a41712fdcefa
ais-one <aaronjxz@gmail.com>
2026-03-15 22:49:55 +0800

    add tests

--- libs/node/tests/keyv.js
+++
@@ -1,4 +0,0 @@
-const StoreKeyV = require('../services/db/keyv')
-const kv = new StoreKeyV({})
-kv.open()
-kv.get()
--- libs/node/utils/aes.js
+++ libs/node/utils/aes.js
@@ -16,24 +16,12 @@ const genIv = () => new Buffer.alloc(16, crypto.pseudoRandomBytes(16))
 
 const genKey = (algorithm, password) => {
   const [size, algo] = algorithm.includes("256") ? [32, 'sha256'] :
-    algorithm.includes("192") ? [32, 'sha192'] :
+    // algorithm.includes("192") ? [32, 'sha192'] : // this is not support
     algorithm.includes("128") ? [32, 'md5'] : []
   const hash = crypto.createHash(algo)
   hash.update(password)
   return new Buffer.alloc(size, hash.digest('hex'),'hex')
 }
-
-const test_aes = (data = 'test data', password = 'pw', algorithm = 'aes256') => {
-  const [iv, key] = [genIv(), genKey(algorithm, password)]
-  const encText = encryptText(algorithm, key, iv, data)
-  const decText = decryptText(algorithm, key, iv, encText)
-  console.log('Password:', password)
-  console.log('Key: ', key.toString(DEFAULT_ENCODING))
-  console.log('Plaintext: ' + decText)
-  console.log('Encrypted (aes256): ' + encText)
-}
-
-if (process?.argv[2] === 'test') test_aes()
 
 module.exports = {
   genIv,
